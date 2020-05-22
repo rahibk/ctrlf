3c43d38049edefe584ae66684872f817056f7cdc
Rahib Kanjiyani <rahibk5498@gmail.com>
2020-05-22 05:54:25 -0400

    changed client endpoint

--- src/Clients/YoutubeClient.js
+++ src/Clients/YoutubeClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getTranscript(videoId) {
   return axios
-    .get("http://video.google.com/timedtext?lang=en&v=" + videoId)
+    .get("https://video.google.com/timedtext?lang=en&v=" + videoId)
     .then(response => response.data)
     .catch(error => console.log(error));
 }
